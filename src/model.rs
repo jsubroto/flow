6aaa50bd97863889a154a7a4f3cfaadb92923a74
Jaimes Subroto <13898099+jsubroto@users.noreply.github.com>
2026-01-16 08:00:48 -0800

    refactor: remove unused model derives

--- src/model.rs
+++ src/model.rs
@@ -1,18 +1,15 @@
-#[derive(Clone, PartialEq, Eq)]
 pub struct Card {
     pub id: String,
     pub title: String,
     pub description: String,
 }
 
-#[derive(Clone, PartialEq, Eq)]
 pub struct Column {
     pub id: String,
     pub title: String,
     pub cards: Vec<Card>,
 }
 
-#[derive(Clone, PartialEq, Eq)]
 pub struct Board {
     pub columns: Vec<Column>,
 }
